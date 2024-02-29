5e070b2ff32885a821b389efa55082f785dc9f3b
Dung Nguyen <ngtdung268@hotmail.com>
2024-02-29 20:02:38 +0700

    Update app name

--- Neo4JHTTPBrowser/Properties/AssemblyInfo.cs
+++ Neo4JHTTPBrowser/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Neo4JHttpBrowser")]
+[assembly: AssemblyTitle("Neo4J HTTP Browser")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Neo4JHttpBrowser")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyProduct("Neo4J HTTP Browser")]
+[assembly: AssemblyCopyright("Copyright © DN 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
