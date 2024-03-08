cad14bf98d17b2b07cf7569a26ca519190c51fd7
Dung Nguyen <ngtdung268@hotmail.com>
2024-03-08 23:31:01 +0700

    Add TransparentPanel

---
+++ Neo4JHTTPBrowser/Controls/TransparentPanel.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+
+namespace Neo4JHTTPBrowser.Controls
+{
+    internal class TransparentPanel : Panel
+    {
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x00000020; // WS_EX_TRANSPARENT
+                return cp;
+            }
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            // base.OnPaintBackground(e);
+        }
+    }
+}
